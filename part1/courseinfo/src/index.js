e794e7b64cbe597bd03e1d69fd36344fc711d89b
Jason Liew <jason.97.liew@gmail.com>
2020-03-08 21:14:36 +1100

    Finish 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const Header = props => (
-	<h1>{props.course}</h1>
-)
+const Header = props => {
+	console.log('Header props:')
+	console.log(props)
+	return (
+		<h1>{props.course}</h1>
+	)
+}
 
 const Content = () => (
 	<div>
@@ -14,9 +18,11 @@ const Content = () => (
 	</div>
 )
 
-const Part = props => (
-	<></>
-)
+const Part = props => {
+	return (
+		<></>
+	)
+}
 
 const Total = () => (
 	<></>
@@ -24,12 +30,18 @@ const Total = () => (
 
 const App = () => {
 	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const part1 = {
+		name: 'Fundamentals of React',
+		exercises: 10
+	}
+	const part2 = {
+		name: 'Using props to pass data',
+		exercises: 7
+	}
+	const part3 = {
+		name: 'State of a component',
+		exercises: 14
+	}
   
 	return (
 		<div>
