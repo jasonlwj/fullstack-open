3d17ef5aba6e8faab9389b04f45268562c117295
Jason Liew <jason.97.liew@gmail.com>
2020-03-08 12:48:04 +1100

    Finish 1.1 and 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = props => (
+	<h1>{props.course}</h1>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Content = () => (
+	<div>
+		<Part />
+		<Part />
+		<Part />
+	</div>
+)
+
+const Part = props => (
+	<></>
+)
+
+const Total = () => (
+	<></>
+)
+
+const App = () => {
+	const course = 'Half Stack application development'
+	const part1 = 'Fundamentals of React'
+	const exercises1 = 10
+	const part2 = 'Using props to pass data'
+	const exercises2 = 7
+	const part3 = 'State of a component'
+	const exercises3 = 14
+  
+	return (
+		<div>
+			<Header course={course}	/>
+			<Content />
+			<Total />
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
