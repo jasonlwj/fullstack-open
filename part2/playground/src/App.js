c85416a0ca1ad73d1088fc729a2304a61527b309
Jason Liew <jason.97.liew@gmail.com>
2020-04-02 17:19:39 +1100

    Finish 2a

---
+++ part2/playground/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Note from './components/Note'
+
+const App = ({ notes }) => (
+	<div>
+		<h1>Notes</h1>
+		<ul>
+			{notes.map(note => 
+				<Note key={note.id} note={note} />
+			)}
+		</ul>
+	</div>
+)
+
+export default App
