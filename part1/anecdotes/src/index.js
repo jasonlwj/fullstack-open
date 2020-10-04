a1a6927fbba8f38ab6d41b5a88174a987a1eddfb
Jason Liew <jason.97.liew@gmail.com>
2020-10-04 23:15:11 +1100

    1.14*: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -21,6 +21,16 @@ const App = ({ anecdotes }) => {
 		5: 0
 	})
 
+	const getTopAnecdoteIndex = () => {
+		let topAnecdoteIndex = 0
+
+		for (let index in votes) {
+			topAnecdoteIndex = (votes[index] > votes[topAnecdoteIndex]) ? index : topAnecdoteIndex
+		}
+
+		return topAnecdoteIndex
+	}
+
 	const handleNextAnecdote = () => {
 		const randomInt = max => Math.floor(Math.random() * Math.floor(max))
 		setSelected(randomInt(anecdotes.length))
@@ -30,16 +40,21 @@ const App = ({ anecdotes }) => {
 		const newVotes = { ...votes }
 		newVotes[selected]++
 		setVotes(newVotes)
+		console.log(getTopAnecdoteIndex())
 	}
 
 	return (
 		<div className="App">
+			<h1>Anecdote of the day</h1>
 			{anecdotes[selected]}<br />
 			has {votes[selected]} votes
 			<div>
 				<Button onClick={handleNextAnecdote} text='next anecdote' />
 				<Button onClick={handleVote} text='vote' />
 			</div>
+			<h1>Anecdote with most votes</h1>
+			{anecdotes[getTopAnecdoteIndex()]} <br />
+			has {votes[getTopAnecdoteIndex()]} votes
 		</div>
 	)
 }
