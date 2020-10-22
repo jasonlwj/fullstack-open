23cf1bdeb78d47bfe6e9eb2559682f60e7f5a8a5
Jason Liew <jason.97.liew@gmail.com>
2020-10-22 19:33:25 +1100

    3.5: Phonebook backend step5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
 	{
 		"name": "Arto Hellas",
@@ -38,6 +40,20 @@ app.delete('/api/persons/:id', (req, res) => {
 	persons = persons.filter(person => person.id !== id)
 
 	res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: Math.floor(Math.random() * 1000)
+	}
+
+	persons = persons.concat(person)
+
+	res.json(person)
 })
 
 app.get('/info', (req, res) => {
