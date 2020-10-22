e5331bc30b2aaec5209fb01d6453823ad0d1caf0
Jason Liew <jason.97.liew@gmail.com>
2020-10-22 19:19:46 +1100

    3.1: Phonebook backend step1

---
+++ part3/phonebook/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+	{
+		"name": "Arto Hellas",
+		"number": "040-123457",
+		"id": 1
+	},
+	{
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523",
+		"id": 2
+	},
+	{
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122",
+		"id": 4
+	}
+]
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+	console.log(`Server now running on port ${PORT}`)
+})
